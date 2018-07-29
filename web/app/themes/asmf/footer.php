1a6fe6ffdb8eef88839f32d58048ef56e7ed8a16
Florent GAILLARD <g.florent44@orange.fr>
2018-07-29 15:26:54 +0200

    Mise en place du thème enfant Ocean WP Mise à jour composer.json Suppresion thème et plugin ASMF

--- web/app/themes/asmf/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-<?php dynamic_sidebar('footer-1') ?>
-</footer>
-<?php wp_footer(); ?>
--- web/app/themes/asmf/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: florent
- * Date: 02/06/18
- * Time: 09:43
- */
