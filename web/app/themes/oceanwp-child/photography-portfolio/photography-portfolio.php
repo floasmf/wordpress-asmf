7c25711d792314a8af1ef56551ef52468a84117d
Florent GAILLARD <g.florent44@orange.fr>
2018-09-16 12:03:58 +0200

    categories photos vidéos

--- web/app/themes/oceanwp-child/photography-portfolio/photography-portfolio.php
+++ web/app/themes/oceanwp-child/photography-portfolio/photography-portfolio.php
@@ -19,6 +19,20 @@ get_header(); ?>
 
             <?php do_action( 'ocean_before_content_inner' ); ?>
 
+            <?php
+            $taxonomy = 'phort_post_category';
+            $portfolio_categories = get_terms(array('taxonomy' => $taxonomy, 'hide_empty ' => false));
+            if ($portfolio_categories) {
+                $html = '<div id="portfolio-categories" class="menu-categories"><ul>';
+                foreach ($portfolio_categories as $portfolio_categorie) {
+                    $html .= sprintf('<li><a href="%s">%s</a></li>', get_term_link($portfolio_categorie->slug, $taxonomy), $portfolio_categorie->name);
+                }
+                $html .= '</ul></div>';
+                echo $html;
+            }
+
+            ?>
+
             <?php phort_load_view(); ?>
 
             <?php do_action( 'ocean_after_content_inner' ); ?>
